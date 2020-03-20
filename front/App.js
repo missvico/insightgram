152b59b4cb00cfef82d1db7ce5571f5b467a6e9e
Victoria Di Liscia <victoria@Air-de-Victoria.utopia.net>
2020-03-20 14:16:33 -0300

    Primer commit para modificar estructura de carpetas

--- front/App.js
+++ front/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Main from './src/components/Main'
+import FeedIconContainer from './src/containers/FeedIconContainer'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Main/>
+      <FeedIconContainer/>
     </View>
   );
 }
