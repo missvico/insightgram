25c62058d9d44df7a36102f34f9701a4d696c829
Victoria Di Liscia <victoria@Air-de-Victoria.utopia.net>
2020-03-20 14:58:39 -0300

    correccion carpeta redux store

--- front/App.js
+++ front/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
-import configureStore from "./src/store/index";
+import configureStore from "./redux/index";
 const store = configureStore();
 
 export default function App() {
