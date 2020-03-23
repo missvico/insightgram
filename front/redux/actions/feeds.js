bfbf14cd180f2a1860ba6b1668455fe72299e3f4
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-23 17:22:26 -0300

    Al home del usuario ya le llega sus feeds y custom para renderizar

--- front/redux/actions/feeds.js
+++ front/redux/actions/feeds.js
@@ -1,26 +1,51 @@
 // import axios from "axios";
-import { SET_FEEDS } from "../constants";
-import axios from 'axios'
-import { allFeedsUrl, ip } from '../../config'
+import { SET_FEEDS, SET_HOME_USER } from "../constants";
+import axios from "axios";
+import { allFeedsUrl, ip, feedsUser } from "../../config";
 
 const setFeeds = feeds => ({
   type: SET_FEEDS,
   feeds
 });
 
+const setHomeUser = data => ({
+  type: SET_HOME_USER,
+  data
+});
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTU4NDk5Mzk2OSwiZXhwIjoxNTg3NTg1OTY5fQ.u2tAW5yI-6NNv4s8uCw3OOOno9_EA1nFZ5hfnas_IYY";
+
 export const fetchAllFeeds = () => dispatch => {
   return axios({
-    method: 'GET',
-    url: `http://${ip+allFeedsUrl}`,
+    method: "GET",
+    url: `http://${ip + allFeedsUrl}`,
     headers: {
-      'Content-Type': 'application/json',
-      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTU4NDk4NTg3MSwiZXhwIjoxNTg3NTc3ODcxfQ.3ZRUgFsSaamu15tiz178WHQunHjAOc9ZaSU1I9SlXYc'
+      "Content-Type": "application/json",
+      token: token
     }
   })
-  .then(response => response.data)
-  .then(feeds => {
-    dispatch(setFeeds(feeds))
-    return feeds
+    .then(response => response.data)
+    .then(feeds => {
+      dispatch(setFeeds(feeds));
+      return feeds;
+    })
+    .catch(err => console.log(err));
+};
+
+export const fetchFeedsByUser = () => dispatch => {
+  return axios({
+    method: "GET",
+    url: `http://${ip + feedsUser}`,
+    headers: {
+      "Content-Type": "application/json",
+      token: token
+    }
   })
-  .catch(err => console.log(err))
-}
+    .then(response => response.data)
+    .then(userHome => {
+      dispatch(setHomeUser(userHome));
+      return userHome;
+    })
+    .catch(err => err);
+};
--- front/src/components/Feeds/FeedsContainer.jsx
+++ front/src/components/Feeds/FeedsContainer.jsx
@@ -5,16 +5,15 @@ import { fetchAllFeeds } from "../../../redux/actions/feeds";
 
 const FeedsContainer = props => {
   const [inputValue, setInputValue] = useState("");
-  const [allFeeds, setAllFeeds] = useState({})
+  const [allFeeds, setAllFeeds] = useState({});
 
   useEffect(() => {
-    console.log("Feeds", Object.keys(allFeeds).length);
-    if(Object.keys(allFeeds).length == 0){
-      props.fetchAllFeeds().then(feeds => setAllFeeds(feeds))
-    }else{
+    if (Object.keys(allFeeds).length == 0) {
+      props.fetchAllFeeds().then(feeds => setAllFeeds(feeds));
+    } else {
       return;
     }
-  }, [setAllFeeds])
+  }, [setAllFeeds]);
 
   const onChange = event => {
     let search = event.nativeEvent.text;
@@ -22,9 +21,11 @@ const FeedsContainer = props => {
     setInputValue(search);
     //props.fetchFeeds(search); en espera de la data
   };
-  
-  return <Feeds onChange={onChange} inputValue={inputValue} feeds={allFeeds.feeds}/>;
 
+  return (
+    <Feeds onChange={onChange} inputValue={inputValue} feeds={allFeeds.feeds} />
+  );
+};
 const mapStateToProps = function(state, ownProps) {
   return {};
 };
