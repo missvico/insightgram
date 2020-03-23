ecc884effb9b925f36ee49787cc8abad4d3377f6
candespeche <53618928+candespeche@users.noreply.github.com>
2020-03-23 19:23:15 -0300

    stack navigator

--- front/App.js
+++ front/App.js
@@ -3,25 +3,32 @@ import { StyleSheet, Text, View } from "react-native";
 import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
 import configureStore from "./redux/index";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import FeedsContainer from "./src/components/Feeds/FeedsContainer";
+import LoginForm from "./src/components/Login/LoginForm";
+import HomescreenContainer from "./src/components/Homescreen/HomescreenContainer.jsx";
 const store = configureStore();
-import FeedsContainer from './src/components/Feeds/FeedsContainer'
-import LoginForm from './src/components/Login/LoginForm'
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-       <FeedsContainer />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomescreenContainer} />
+          <Stack.Screen name="Feeds" component={FeedsContainer} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    padding: 20
   }
 });
--- front/src/components/Homescreen/HomescreenContainer.jsx
+++ front/src/components/Homescreen/HomescreenContainer.jsx
@@ -1,115 +1,118 @@
 import React from "react";
-import {Text} from "react-native"
+import { Text } from "react-native";
+import { connect } from "react-redux";
+import Homescreen from "./Homescreen";
 
-
-export default function HomescreenContainer() {
+const HomescreenContainer = ({ navigation }) => {
+  const handlePress = () => {
+    navigation.navigate("Feeds");
+  };
   const data = {
-    "feeds":{
-       "custom":[
-          {
-             "id":25,
-             "name":"Pelotita 1",
-             "has_pending_stories":true,
-             "stories":[
-                {
-                   "id":27,
-                   "status":"not_seen"
-                },
-                {
-                   "id":28,
-                   "status":"not_seen"
-                },
-                {
-                   "id":29,
-                   "status":"seen"
-                }
-             ]
-          }
-       ],
-       "myFeeds":[
-          {
-             "id":25,
-             "name":"Pelotita 1",
-             "has_pending_stories":true,
-             "stories":[
-                {
-                   "id":27,
-                   "status":"not_seen"
-                },
-                {
-                   "id":28,
-                   "status":"not_seen"
-                },
-                {
-                   "id":29,
-                   "status":"seen"
-                }
-             ]
-          },
-          {
-             "id":26,
-             "name":"Pelotita 2",
-             "has_pending_stories":true,
-             "stories":[
-                {
-                   "id":30,
-                   "status":"not_seen"
-                },
-                {
-                   "id":31,
-                   "status":"not_seen"
-                },
-                {
-                   "id":32,
-                   "status":"seen"
-                }
-             ]
-          }
-       ],
-       "discover":[
-          {
-             "id":27,
-             "name":"Pelotita 3",
-             "has_pending_stories":true,
-             "stories":[
-                {
-                   "id":33,
-                   "status":"not_seen"
-                },
-                {
-                   "id":34,
-                   "status":"not_seen"
-                },
-                {
-                   "id":35,
-                   "status":"seen"
-                }
-             ]
-          },
-          {
-             "id":28,
-             "name":"Pelotita 4",
-             "has_pending_stories":true,
-             "stories":[
-                {
-                   "id":36,
-                   "status":"not_seen"
-                },
-                {
-                   "id":37,
-                   "status":"not_seen"
-                },
-                {
-                   "id":38,
-                   "status":"seen"
-                }
-             ]
-          }
-       ]
+    feeds: {
+      custom: [
+        {
+          id: 25,
+          name: "Pelotita 1",
+          has_pending_stories: true,
+          stories: [
+            {
+              id: 27,
+              status: "not_seen"
+            },
+            {
+              id: 28,
+              status: "not_seen"
+            },
+            {
+              id: 29,
+              status: "seen"
+            }
+          ]
+        }
+      ],
+      myFeeds: [
+        {
+          id: 25,
+          name: "Pelotita 1",
+          has_pending_stories: true,
+          stories: [
+            {
+              id: 27,
+              status: "not_seen"
+            },
+            {
+              id: 28,
+              status: "not_seen"
+            },
+            {
+              id: 29,
+              status: "seen"
+            }
+          ]
+        },
+        {
+          id: 26,
+          name: "Pelotita 2",
+          has_pending_stories: true,
+          stories: [
+            {
+              id: 30,
+              status: "not_seen"
+            },
+            {
+              id: 31,
+              status: "not_seen"
+            },
+            {
+              id: 32,
+              status: "seen"
+            }
+          ]
+        }
+      ],
+      discover: [
+        {
+          id: 27,
+          name: "Pelotita 3",
+          has_pending_stories: true,
+          stories: [
+            {
+              id: 33,
+              status: "not_seen"
+            },
+            {
+              id: 34,
+              status: "not_seen"
+            },
+            {
+              id: 35,
+              status: "seen"
+            }
+          ]
+        },
+        {
+          id: 28,
+          name: "Pelotita 4",
+          has_pending_stories: true,
+          stories: [
+            {
+              id: 36,
+              status: "not_seen"
+            },
+            {
+              id: 37,
+              status: "not_seen"
+            },
+            {
+              id: 38,
+              status: "seen"
+            }
+          ]
+        }
+      ]
     }
- }
+  };
+  return <Homescreen handlePress={handlePress} />;
+};
 
-  return (
-    <Text>Homescreen</Text>
-  );
-}
+export default connect(null, null)(HomescreenContainer);
