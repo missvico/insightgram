ff333ab06298d91ae3da4c45dc43d2947cece7a5
caroocat <karithofernandez@gmail.com>
2020-03-24 18:39:17 -0300

    homeScreen hecho

--- front/src/components/Homescreen/HomescreenContainer.jsx
+++ front/src/components/Homescreen/HomescreenContainer.jsx
@@ -5,10 +5,12 @@ import { fetchFeedsByUser } from "../../../redux/actions/feeds";
 import Homescreen from "./Homescreen";
 
 const HomescreenContainer = ({ navigation, fetchFeedsByUser }) => {
+  const [userHome, setUserHome] = useState({}); //Feeds y Customs del usuario
+  const [feeds, setFeeds] = useState([]);
+  //Titulo de la screen
   const handlePress = () => {
     navigation.navigate("Feeds");
   };
-  const [userHome, setUserHome] = useState({}); //Feeds y Customs del usuario
 
   useEffect(() => {
     if (Object.keys(userHome).length == 0) {
@@ -16,8 +18,30 @@ const HomescreenContainer = ({ navigation, fetchFeedsByUser }) => {
     } else {
       return;
     }
+    // historias o feeds hardcode  !sigan la estructura de datos!
+    setFeeds([
+      {
+        group: "Custom",
+        feeds: [
+          { is_suscribed: false, id: 1 },
+          { is_suscribed: false, id: 2 },
+          { is_suscribed: false, id: 3 },
+          { is_suscribed: false, id: 4 }
+        ],
+        id: 1
+      },
+      {
+        group: "my Feeds",
+        feeds: [
+          { is_suscribed: false, id: 1 },
+          { is_suscribed: false, id: 2 },
+          { is_suscribed: false, id: 3 }
+        ],
+        id: 2
+      }
+    ]);
   }, [setUserHome]);
-  return <Homescreen handlePress={handlePress} />;
+  return <Homescreen handlePress={handlePress} feeds={feeds} />;
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
