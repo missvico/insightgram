0c88154e107e04339b1f65d5c45d1da27da8be7c
candespeche <53618928+candespeche@users.noreply.github.com>
2020-03-24 11:08:34 -0300

    stack navigation bug

--- front/src/components/Feeds/FeedsContainer.jsx
+++ front/src/components/Feeds/FeedsContainer.jsx
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import Feeds from "./Feeds";
-import { fetchAllFeeds } from "../../../redux/actions/feeds";
+// import { fetchAllFeeds } from "../../../redux/actions/feeds";
 
 const FeedsContainer = props => {
   const [inputValue, setInputValue] = useState("");
-  const [allFeeds, setAllFeeds] = useState({})
+  const [allFeeds, setAllFeeds] = useState({});
 
-  useEffect(() => {
-    console.log("Feeds", Object.keys(allFeeds).length);
-    if(Object.keys(allFeeds).length == 0){
-      props.fetchAllFeeds().then(feeds => setAllFeeds(feeds))
-    }else{
-      return;
-    }
-  }, [setAllFeeds])
+  // useEffect(() => {
+  //   if (Object.keys(allFeeds).length == 0) {
+  //     props.fetchAllFeeds().then(feeds => setAllFeeds(feeds));
+  //   } else {
+  //     return;
+  //   }
+  // }, [setAllFeeds]);
 
   const onChange = event => {
     let search = event.nativeEvent.text;
     console.log(search);
     setInputValue(search);
-    //props.fetchFeeds(search); en espera de la data
   };
-  
-  return <Feeds onChange={onChange} inputValue={inputValue} feeds={allFeeds.feeds}/>;
 
+  return (
+    <Feeds onChange={onChange} inputValue={inputValue} feeds={allFeeds.feeds} />
+  );
+};
 const mapStateToProps = function(state, ownProps) {
   return {};
 };
 
 const mapDispatchToProps = function(dispatch, ownProps) {
   return {
-    fetchFeeds: search => dispatch(fetchFeeds(search)),
-    fetchAllFeeds: () => dispatch(fetchAllFeeds())
+    // fetchFeeds: search => dispatch(fetchFeeds(search)),
+    // fetchAllFeeds: () => dispatch(fetchAllFeeds())
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(FeedsContainer);
+export default connect(null, null)(FeedsContainer);
