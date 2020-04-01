0af883eb3aef9119a8b2051591d2f4d1a4e6db40
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-04-01 19:40:44 -0300

    En redux cambia el estado de los feeds y las historias.

--- front/redux/constants.js
+++ front/redux/constants.js
@@ -1,3 +1,4 @@
 export const SET_FEEDS = "SET_FEEDS";
 export const SET_HOME_USER = "SET_HOME_USER";
+export const UPDATE_HOME_DATA = "UPDATE_HOME_DATA";
 export const LOGIN_USER = "LOGIN_USER";
--- front/src/components/Stories/StoriesContainer.jsx
+++ front/src/components/Stories/StoriesContainer.jsx
@@ -6,28 +6,43 @@ import Header from "./Content/Header/Header";
 export default StoriesContainer = ({ handleClose, feed, handleFeedChange }) => {
   const { stories } = feed;
   const [index, setIndex] = useState(0);
-  const [storiesSeen, setStoriesSeen] = useState([]);
+  const [currentStory, setCurrentStory] = useState({});
+
+  useEffect(() => {
+    const inx = lastNotSeen(stories);
+
+    if (inx !== -1) {
+      setIndex(inx);
+      setCurrentStory(stories[inx]);
+      changeStatus(inx);
+    } else {
+      setCurrentStory(stories[0]);
+      setIndex(stories.length - 1);
+    }
+  }, [setCurrentStory, feed]);
 
   const handleStoryChange = n => {
-    changeStatus(index);
-    if (index + n < 0 || index + n === stories.length) {
+    if (index + n < 0 || index + n > stories.length) {
       setIndex(0);
       handleFeedChange(n);
     } else {
+      changeStatus(index);
       setIndex(index + n);
+      setCurrentStory(stories[index]);
     }
   };
 
-  const changeStatus = inx => {
-    stories[inx].status = "seen";
-    storiesSeen.push(stories[inx].id);
-    setStoriesSeen(storiesSeen);
+  const changeStatus = index => {
+    stories[index].status = "seen";
   };
 
   return (
     <View flex={1}>
       <Header style={{ position: "absolute" }} handleClose={handleClose} />
-      <Story story={stories[index]} handleStoryChange={handleStoryChange} />
+      <Story story={currentStory} handleStoryChange={handleStoryChange} />
     </View>
   );
 };
+
+const lastNotSeen = stories =>
+  stories.indexOf(stories.filter(story => story.status === "not_seen")[0]);
