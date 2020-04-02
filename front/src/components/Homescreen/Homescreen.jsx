3f4bf776cd961ef2aadcf0afe0fd8eff1421a318
Jose Daniel <jodaro2013@gmail.com>
2020-04-02 16:09:41 -0300

    Discover movido a common para usarlo en varias partes

--- front/src/components/Homescreen/Homescreen.jsx
+++ front/src/components/Homescreen/Homescreen.jsx
@@ -4,18 +4,30 @@ import { View, ScrollView } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-import Discover from "./Discover/Discover";
-import FeedList from "../Common/FeedList/FeedList"
+import Discover from "../Common/Discover/Discover";
+import FeedList from "../Common/FeedList/FeedList";
 
 export default ({ handlePress, feeds, handleStory }) => {
   return (
     <View>
       <ScrollView>
-        <FeedList title={"Custom"} feeds={feeds.custom} disableTick={true} handleStory={handleStory} section={"custom"}/>
-        <FeedList title={"myFeeds"} feeds={feeds.all} disableTick={true} handleStory={handleStory} section={"all"}/>
-        <Discover feeds={feeds.discover} handleStory={handleStory} />
+        <FeedList
+          title={"Custom"}
+          feeds={feeds.custom}
+          disableTick={true}
+          handleStory={handleStory}
+          section={"custom"}
+        />
+        <FeedList
+          title={"myFeeds"}
+          feeds={feeds.all}
+          disableTick={true}
+          handleStory={handleStory}
+          section={"all"}
+        />
+        <Discover feeds={feeds.discover} handleStory={handleStory} grid={3} />
       </ScrollView>
-      
+
       <View
         style={{
           display: "flex",
@@ -27,7 +39,7 @@ export default ({ handlePress, feeds, handleStory }) => {
         }}
       >
         <TouchableWithoutFeedback onPress={handlePress}>
-          <AntDesign name="pluscircle" size={35} color="#01ADED" />
+          <AntDesign name='pluscircle' size={35} color='#01ADED' />
         </TouchableWithoutFeedback>
       </View>
     </View>
