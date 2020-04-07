89de137dbeafa682c9daca09450d996ca3c04434
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-04-07 15:07:50 -0300

    Update Feeds.jsx

--- front/src/components/Feeds/Feeds.jsx
+++ front/src/components/Feeds/Feeds.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Text, View, ScrollView, SafeAreaView, FlatList } from "react-native";
 import { Search, Done, ButtonText } from "./style";
 import FeedList from "../Common/FeedList/FeedList";
-import AllFeedsToggle from "./AllFeedsToggle";
-import ToggleTab from "./ToggleTab";
-import Navigation from "./Navigation";
+import AllFeedsToggle from "../Subscribe/AllFeedsToggle";
+import ToggleTab from "../Subscribe/ToggleTab";
+import Navigation from "../Subscribe/Navigation";
 
 export default ({ onChange, inputValue, feeds, handlePress }) => {
   return (
